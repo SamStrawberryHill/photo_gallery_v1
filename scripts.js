c0a051428a665939cc7770cee095362dfc0aeef3
Samantha Hill <samhill5643@gmail.com>
2016-03-31 13:05:25 -0400

    Javascript Fix
    simplifying the javascript

---
+++ scripts.js
@@ -0,0 +1,31 @@
+var $overlay = $('<div> id = "overlay"></div>');
+var image = $("<img>");
+var $caption = $("<p></p>");
+
+//image to overlay
+$image.append($image);
+
+//caption to overlay
+$overlay.append($caption);
+
+//add overlay
+$("body").append($overlay);
+
+//click event on link to an image
+$("imageGallery a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  //update overlay with imageLocation
+  $image.attr("src", imageLocation);
+  //show overlay
+  $overlay.show();
+  //get child caption
+  var captionText= $(this).children("img").attr("<p></p>");
+  $caption.text(captionText);
+});
+
+//overlay clicked
+$overlay.click(function() {
+  //hide overlay
+  $overlay.hide();
+});

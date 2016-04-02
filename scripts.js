f8db145d981d09e01b984e0c3cc32bdef0a27ab1
Samantha Hill <samhill5643@gmail.com>
2016-04-02 11:45:36 -0400

    Li fix

--- scripts.js
+++ scripts.js
@@ -1,34 +1,34 @@
 
 $(document).ready(function() {
-var $overlay = $('<div> id="overlay"></div>');
-var $image = $("<img>");
-var $caption = $("<p></p>");
+  var $overlay = $('<div id="overlay"></div>');
+  var $image = $("<img>");
+  var $caption = $("<p></p>");
 
-//image to overlay
-$image.append($image);
+  //image to overlay
+  $image.append($image);
 
-//caption to overlay
-$overlay.append($caption);
+  //caption to overlay
+  $overlay.append($caption);
 
-//add overlay
-$("body").append($overlay);
+  //add overlay
+  $("body").append($overlay);
 
-//click event on link to an image
-$("imageGallery a").click(function(event){
-  event.preventDefault();
-  var imageLocation = $(this).attr("href");
-  //update overlay with imageLocation
-  $image.attr("src", imageLocation);
-  //show overlay
-  $overlay.show();
-  //get child's alt attribute and set caption
-  var captionText= $(this).children("img").attr("alt");
-  $caption.text(captionText);
-});
+  //click event on link to an image
+  $("#imageGallery a").click(function(event){
+    event.preventDefault();
+    var imageLocation = $(this).attr("href");
+    //update overlay with imageLocation
+    $image.attr("src", imageLocation);
+    //show overlay
+    $overlay.show();
+    //get child's alt attribute and set caption
+    var captionText= $(this).children("img").attr("alt");
+    $caption.text(captionText);
+  });
 
-//overlay clicked
-$overlay.click(function() {
-  //hide overlay
-  $overlay.hide();
-});
+  //overlay clicked
+  $overlay.click(function() {
+    //hide overlay
+    $overlay.hide();
+  });
 });
